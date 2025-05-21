e8e9b53b2034491b22275065647617ae959ce856
tvenceslau <tiago.venceslau@pdmfc.com>
2025-05-21 19:19:28 +0100

    DECAF-125 decorator extensions

--- src/PouchRepository.ts
+++ src/PouchRepository.ts
@@ -2,9 +2,11 @@ import { Model } from "@decaf-ts/decorator-validation";
 import { MangoQuery } from "@decaf-ts/for-couchdb";
 import { Adapter, Repository } from "@decaf-ts/core";
 import Database = PouchDB.Database;
+import { PouchFlags } from "./types";
+import { Context } from "@decaf-ts/db-decorators";
 
 export type PouchRepository<M extends Model> = Repository<
   M,
   MangoQuery,
-  Adapter<Database, MangoQuery>
+  Adapter<Database, MangoQuery, PouchFlags, Context<PouchFlags>>
 >;
