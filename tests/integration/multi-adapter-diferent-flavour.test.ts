489b492a7c3e0a11da8810f0d4a63b502f87a190
tvenceslau <venceslau.tiago@gmail.com>
2025-12-04 17:26:52 +0000

    `DECAF-204 - Add unit and integration tests for multi-adapter and flavour handling, enhance decoration metadata cloning.`

---
+++ tests/integration/multi-adapter-diferent-flavour.test.ts
@@ -0,0 +1,149 @@
+import { Metadata, uses } from "@decaf-ts/decoration";
+import { RamAdapter, RamFlavour, Adapter } from "@decaf-ts/core";
+RamAdapter.decoration();
+Adapter.setCurrent(RamFlavour);
+
+import {
+  Model,
+  model,
+  type ModelArg,
+  required,
+} from "@decaf-ts/decorator-validation";
+import { createdBy, Observer, pk, Repository } from "@decaf-ts/core";
+import { ServerScope } from "nano";
+import { PouchAdapter, PouchFlavour } from "../../src/index";
+import { ConflictError } from "@decaf-ts/db-decorators";
+import { getHttpPouch } from "../pouch";
+import { NanoAdapter } from "@decaf-ts/for-nano";
+
+@uses(RamFlavour)
+@model()
+class Model1 extends Model {
+  @pk({ type: "Number", generated: true })
+  id1!: number;
+
+  @required()
+  name1!: string;
+
+  @createdBy()
+  owner1!: string;
+
+  constructor(arg?: ModelArg<Model1>) {
+    super(arg);
+  }
+}
+@uses(PouchFlavour)
+@model()
+class Model2 extends Model {
+  @pk({ type: "Number", generated: true })
+  id2!: number;
+
+  @required()
+  name2!: string;
+
+  @createdBy()
+  owner2!: string;
+
+  constructor(arg?: ModelArg<Model2>) {
+    super(arg);
+  }
+}
+
+const admin = "couchdb.admin";
+const admin_password = "couchdb.admin";
+const user = "couchdb.admin";
+const user_password = "couchdb.admin";
+const dbName = "test_db_multi_flavour";
+const dbHost = "localhost:10010";
+
+jest.setTimeout(50000);
+
+describe("Adapter Integration", () => {
+  let con: ServerScope;
+  let adapter: NanoAdapter;
+  // let repo: NanoRepository<TestModel>;
+
+  beforeAll(async () => {
+    con = await NanoAdapter.connect(admin, admin_password, dbHost);
+    expect(con).toBeDefined();
+    try {
+      await NanoAdapter.createDatabase(con, dbName);
+      await NanoAdapter.createUser(con, dbName, user, user_password);
+    } catch (e: any) {
+      if (!(e instanceof ConflictError)) throw e;
+    }
+    adapter = new NanoAdapter({
+      user: user,
+      password: user_password,
+      host: dbHost,
+      dbName: dbName,
+    });
+  });
+
+  let observer: Observer;
+  let mock: any;
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+    jest.resetAllMocks();
+    mock = jest.fn();
+    observer = new (class implements Observer {
+      refresh(...args: any[]): Promise<void> {
+        return mock(...args);
+      }
+    })();
+    // repo.observe(observer);
+  });
+
+  afterEach(() => {
+    // repo.unObserve(observer);
+  });
+
+  afterAll(async () => {
+    await NanoAdapter.deleteDatabase(con, dbName);
+  });
+
+  let ram1: RamAdapter;
+  let ram2: PouchAdapter;
+
+  it("displays the correct decoration for model1", () => {
+    const meta1 = Metadata.get(Model1);
+    const meta2 = Metadata.get(Model2);
+    //
+    // expect(meta1.operations.owner1.on.create.handlers.Model1.owner1);
+    // console.log(meta1);
+  });
+
+  it("initializes adapters correctly", async () => {
+    ram1 = new RamAdapter();
+    ram2 = await getHttpPouch(dbName, user, user_password);
+  });
+
+  it("Reads default flavour correctly", async () => {
+    const repo1 = Repository.forModel(Model1);
+    expect(repo1).toBeDefined();
+    expect(repo1["adapter"]).toBeInstanceOf(RamAdapter);
+    const repo2 = Repository.forModel(Model2);
+    expect(repo2).toBeDefined();
+    expect(repo2["adapter"]).toBeInstanceOf(PouchAdapter);
+    const created1 = await repo1.create(
+      new Model1({
+        name1: "test1",
+      })
+    );
+
+    expect(created1).toBeDefined();
+    expect(created1.hasErrors()).toBeUndefined();
+    expect(created1.owner1).toEqual(expect.any(String));
+
+    const created2 = await repo2.create(
+      new Model2({
+        name2: "test2",
+      })
+    );
+
+    expect(created2).toBeDefined();
+    expect(created2.hasErrors()).toBeUndefined();
+    expect(created2.owner2).toEqual(user);
+  });
+});
